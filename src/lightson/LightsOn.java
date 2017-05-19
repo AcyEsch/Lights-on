76b7c21ee596dd00dc2ee7a7cf1e5c7ca914fd72
JasminWollgarten <jasmin.wollgarten@gmail.com>
2017-05-19 14:20:30 +0200

    Update LightsOn.java

--- src/lightson/LightsOn.java
+++ src/lightson/LightsOn.java
@@ -5,23 +5,51 @@
  */
 package lightson;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
- * @author Acy
+ * @author Acy  Jasmin
  */
-public class LightsOn extends Application {
+public class LightsOn extends Application 
+{
     
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception 
+    {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLightsOn.fxml"));
         
         Scene scene = new Scene(root);
+        
+        
+        double radius = 20;
+        double height = 800;                                                    // Größe des Fensters           
+        double width = 500;                                                     //Und Bereich in der sich die Kugel bewegen kann
+        
+       Kugel kugel = new Kugel(radius, height, width);
+       
+       KeyFrame k=new KeyFrame(Duration.millis(10),
+            e->
+            {
+                
+                    kugel.move();
+                
+            });
+       
+       Timeline t= new Timeline(k);
+            t.setCycleCount(Timeline.INDEFINITE);
+            t.play();
+       
+
+        
+        
         
         stage.setScene(scene);
         stage.show();
@@ -30,8 +58,8 @@ public class LightsOn extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch(args);
-    }
-    
+    }   
 }
