a64755c440ea2c3d850e205b9a1fd97acfb3fc83
JasminWollgarten <jasmin.wollgarten@gmail.com>
2017-06-15 14:14:38 +0200

    Create DragDrop.java

--- src/logic/DragDrop.java
+++ src/logic/DragDrop.java
@@ -1,54 +1,11 @@
 package logic;
 import javafx.scene.Cursor;
+import javafx.scene.paint.Color;
 
 public class DragDrop 
 {
     private double orgSceneX, orgSceneY;
-       /*
-       //DragAndDrop
-     kugel.setCursor(Cursor.HAND);
-    kugel.setOnMousePressed((t) -> {
-      orgSceneX = t.getSceneX();
-      orgSceneY = t.getSceneY();
-      Kugel k = (Kugel) (t.getSource());
-      k.toFront();
-    });
-    kugel.setOnMouseDragged((t) -> {
-      double offsetX = t.getSceneX() - orgSceneX;
-      double offsetY = t.getSceneY() - orgSceneY;
-      Kugel k = (Kugel) (t.getSource());
-      k.setCenterX(k.getCenterX() + offsetX);
-      k.setCenterY(k.getCenterY() + offsetY);
-      
-      orgSceneX = t.getSceneX();
-      orgSceneY = t.getSceneY();      
-    });
-       
-    bahn.setCursor(Cursor.HAND);
-    bahn.setOnMousePressed((t) -> {
-      orgSceneX = t.getSceneX();
-      orgSceneY = t.getSceneY();
-      Bahn b = (Bahn) (t.getSource());
-      b.toFront();
-    });
-    bahn.setOnMouseDragged((t) -> {
-      double offsetX = t.getSceneX() - orgSceneX;
-      double offsetY = t.getSceneY() - orgSceneY;
-      Bahn b = (Bahn) (t.getSource());
-      //b.setCenterX(b.getCenterX() + offsetX);
-     // b.setCenterY(b.getCenterY() + offsetY);
-     b.setStartX(b.getStartX() + offsetX);
-     b.setStartY(b.getStartY() + offsetY);
-     b.setEndX(b.getEndX() + offsetX);
-     b.setEndY(b.getEndY() + offsetY);
-     
-     b.werteBerechnen();
-     
-      orgSceneX = t.getSceneX();
-      orgSceneY = t.getSceneY();      
-    });   
-      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-    */
+    private Bahn lastBahn = new Bahn();
     
     public void dragKugel(Kugel kugel)
     {
@@ -85,16 +42,23 @@ public class DragDrop
       orgSceneY = t.getSceneY();
 
       Bahn b = (Bahn) (t.getSource());
+      
+      if(lastBahn != b)
+      {
+        lastBahn.setStroke(Color.BLACK);      
+        lastBahn = b;
+      }
+      
+      
       b.toFront();
+      
+      b.setStroke(Color.GREEN);            
     });
     bahn.setOnMouseDragged((t) -> {
       double offsetX = t.getSceneX() - orgSceneX;
       double offsetY = t.getSceneY() - orgSceneY;
 
       Bahn b = (Bahn) (t.getSource());
-
-      //b.setCenterX(b.getCenterX() + offsetX);
-     // b.setCenterY(b.getCenterY() + offsetY);
 
      b.setStartX(b.getStartX() + offsetX);
      b.setStartY(b.getStartY() + offsetY);
@@ -104,8 +68,11 @@ public class DragDrop
      b.werteBerechnen();
      
       orgSceneX = t.getSceneX();
-      orgSceneY = t.getSceneY();      
+      orgSceneY = t.getSceneY();              
     });   
     }
-    
+    public Bahn getBahn()
+    {
+        return lastBahn;
+    }   
 }
