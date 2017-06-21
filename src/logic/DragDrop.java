f822d686fd636ca80d0792909249cd123ad4c456
JasminWollgarten <jasmin.wollgarten@gmail.com>
2017-06-21 15:53:01 +0200

    Add files via upload

---
+++ src/logic/DragDrop.java
@@ -0,0 +1,96 @@
+package logic;
+import javafx.scene.Cursor;
+import javafx.scene.paint.Color;
+
+public class DragDrop 
+{
+    private double orgSceneX, orgSceneY;
+    private Bahn lastBahn = new Bahn();
+    private boolean canDrag = true;
+    
+    public void dragKugel(Kugel kugel)
+    {
+        kugel.setCursor(Cursor.HAND);
+
+    kugel.setOnMousePressed((t) -> {
+        if(canDrag)
+        {
+      orgSceneX = t.getSceneX();
+      orgSceneY = t.getSceneY();
+
+      Kugel k = (Kugel) (t.getSource());
+      k.toFront();
+        }
+    });
+    kugel.setOnMouseDragged((t) -> {
+        if(canDrag)
+        {
+      double offsetX = t.getSceneX() - orgSceneX;
+      double offsetY = t.getSceneY() - orgSceneY;
+
+      Kugel k = (Kugel) (t.getSource());
+
+      k.setCenterX(k.getCenterX() + offsetX);
+      k.setCenterY(k.getCenterY() + offsetY);
+
+      
+      orgSceneX = t.getSceneX();
+      orgSceneY = t.getSceneY();     
+        }
+    });
+    }
+    
+    public void dragBahn(Bahn bahn)
+    {
+        bahn.setCursor(Cursor.HAND);
+
+    bahn.setOnMousePressed((t) -> {
+        if(canDrag)
+        {
+      orgSceneX = t.getSceneX();
+      orgSceneY = t.getSceneY();
+
+      Bahn b = (Bahn) (t.getSource());
+      
+      if(lastBahn != b)
+      {
+        lastBahn.setStroke(Color.BLACK);      
+        lastBahn = b;
+      }
+      
+      
+      b.toFront();
+      
+      b.setStroke(Color.GREEN);   
+        }
+    });
+    bahn.setOnMouseDragged((t) -> {
+        if(canDrag)
+        {
+      double offsetX = t.getSceneX() - orgSceneX;
+      double offsetY = t.getSceneY() - orgSceneY;
+
+      Bahn b = (Bahn) (t.getSource());
+
+     b.setStartX(b.getStartX() + offsetX);
+     b.setStartY(b.getStartY() + offsetY);
+     b.setEndX(b.getEndX() + offsetX);
+     b.setEndY(b.getEndY() + offsetY);
+     
+     b.werteBerechnen();
+     
+      orgSceneX = t.getSceneX();
+      orgSceneY = t.getSceneY();  
+        }
+    });   
+    }
+    public Bahn getBahn()
+    {
+        return lastBahn;
+    }
+    
+    public void setCanDrag(boolean cd)
+    {
+        canDrag = cd;
+    }
+}
